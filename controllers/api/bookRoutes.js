dfaa76cc1e06f8e9d70b157e5adb78f6ceec0666
Zoooe-Brooo <zhongwenli.zoe@gmail.com>
2024-09-09 16:47:36 +0930

    Update home/api  each router's details, add JS files in public/js, add Review model file, add reviewData.json in seeds

--- controllers/api/bookRoutes.js
+++ controllers/api/bookRoutes.js
@@ -1,13 +1,78 @@
 const router = require("express").Router();
-const { User } = require("../../models");
-const { Book } = require("../../models");
-const { Review } = require("../../models");
+const { User, Book, Review } = require("../../models");
 const withAuth = require("../../utils/auth");
 
-// can only add new comments if logged in
-router.post("/:book_id", withAuth, async (req, res) => {});
+// can only add new reviews if logged in, need debug
+router.post("/:id", withAuth, async (req, res) => {
+  try {
+    const newReview = await Review.create({
+      ...req.body,
+      user_id: req.session.user_id,
+      book_id: req.params.book_id,
+    });
 
-// can only delete comments if logged in
-router.delete("/:book_id/:review_id", withAuth, async (req, res) => {});
+    res.status(200).json(newReview);
+
+    res.render("bookpage", {
+      ...newReview,
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// can only update reviews if logged in, need debug
+router.put("/review/:id", withAuth, async (req, res) => {
+  try {
+    const reviewData = await Review.update(req.body, {
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+        book_id: req.params.book_id,
+      },
+    });
+
+    if (!reviewData[0]) {
+      res.status(404).json({ message: "No review found with this id!" });
+      return;
+    }
+
+    res.status(200).json(reviewData);
+
+    const review = reviewData.get({ plain: true });
+    res.render("bookpage", {
+      ...review,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// can only delete reviews if logged in, need debug
+router.delete("/review/:id", withAuth, async (req, res) => {
+  try {
+    const reviewData = await Review.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+        book_id: req.params.book_id,
+      },
+    });
+
+    if (!reviewData) {
+      res.status(404).json({ message: "No review found with this id!" });
+      return;
+    }
+
+    res.status(200).json(reviewData);
+
+    const review = reviewData.get({ plain: true });
+    res.render("bookpage", {
+      ...review,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 const bookRoutes = require("./bookRoutes");
 
-router.use("/users", userRoutes);
-router.use("/books", bookRoutes);
+router.use("/user", userRoutes);
+router.use("/book", bookRoutes);
 
 module.exports = router;
