e19532e4f4f1b3653374bd1fa4ce2358a7ff769a
Leon <tranhoangquan44@gmail.com>
2024-09-15 12:05:28 +1000

    increase session cookie to 50 mins

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const hbs = exphbs.create({ helpers });
 const sess = {
   secret: "secret string",
   cookie: {
-    maxAge: 300000,
+    maxAge: 3000000,
     httpOnly: true,
     secure: false,
     sameSite: "strict",
